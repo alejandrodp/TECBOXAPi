fe4adb0ac80bfd00bcc16f04b537f31204879e8f
Alejandro <diaza913@gmail.com>
2020-07-17 14:09:46 -0600

    tesgin

---
+++ Controllers/LoggingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RequestResponseLoggingDemo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggingController : Controller
+    {
+        // GET: api/<LoggingController>
+        public IActionResult Logging()
+        {
+            MemoryStream ms = new MemoryStream();
+            using (FileStream file = new FileStream("file.txt", FileMode.Open, FileAccess.Read))
+            {
+                byte[] bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+                ms.Write(bytes, 0, (int)file.Length);
+
+
+            }
+
+
+            string result = "Hello world! Time is: " + DateTime.Now;
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(result, System.Text.Encoding.UTF8, "text/plain");
+            return File(ms.ToArray(), "text/plain");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TECBoxAPI.Middleware;
 
 namespace TECBoxAPI
 {
@@ -25,6 +19,7 @@ namespace TECBoxAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddControllers();
         }
 
@@ -35,6 +30,8 @@ namespace TECBoxAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
 
             app.UseHttpsRedirection();
 
